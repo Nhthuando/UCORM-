f32c6bd336e354e33c2e3f9cc752186e79e660c6
Nhthuando <thuanhuugl@gmail.com>
2026-05-20 19:26:54 +0700

    add logout + add cors render deploy

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import authRouter from "./src/routes/authenRoutes.js";
 import placeRouter from "./src/routes/placeRoutes.js";
 import AIRouter from "./src/routes/AIRoutes.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -13,6 +14,22 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/places", placeRouter);
 app.use("/api/reviews", AIRouter);
+
+const allowedOrigins = [
+    "https://ucorm.onrender.com",
+    "http://localhost:5173"
+];
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) return callback(null, true);
+        return callback(new Error("Not allowed by CORS"));
+        },
+        credentials: true,
+    })
+);
 
 app.listen(PORT,  () => {
     console.log("-----------------------------------------------");
